083a9d97d442cebaa9f2353a003cff810fbc9c1b
Shirin alizadeh <shirin.alizadeh19@gmail.com>
2024-12-27 13:12:14 -0800

    develop

--- Shop.Domain/Entities/Product.cs
+++ Shop.Domain/Entities/Product.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Shop.Domain.Entities
+﻿namespace Shop.Domain.Entities
 {
     public abstract class Product
     {
