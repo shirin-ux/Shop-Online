94e210c12b34e89028c2d2e0e8be8a198508f92b
Shirin alizadeh <shirin.alizadeh19@gmail.com>
2025-04-11 11:28:48 -0700

    Add Command and handler for module product

---
+++ Shop.Application/Commands/CreateProductHandler.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using Shop.Domain.Interface;
+
+namespace Shop.Application.Commands
+{
+    public class CreateProductHandler(IApplicationDbContext context) : IRequestHandler<CreateProductCommand, int>
+    {
+        private readonly IApplicationDbContext _context = context;
+        public async Task<int> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        {
+            var product = new Domain.Entities.Product
+            {
+                CategoryId = request.CategoryId,
+                Name = request.Name,
+                Price = request.Price
+            };
+            _context.Products.Add(product);
+            await _context.SaveChangesAsync(cancellationToken);
+            return product.Id;
+        }
+    }
+}
--- Shop.Application/DTOs/ProductDto.cs
+++ Shop.Application/DTOs/ProductDto.cs
@@ -10,6 +10,6 @@ namespace Shop.Application.DTOs
     {
         public string Name { get; set; }
         public decimal Price { get; set; }
-        public string ProductType { get; set; }
+        public int CategoryId { get; set; }
     }
 }
